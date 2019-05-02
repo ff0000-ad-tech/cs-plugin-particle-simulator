5a78b9062096599f961cdfbeca4375f45ae80e5b
Jacques Altounian <cattlebane@gmail.com>
2019-05-02 12:05:24 -0700

    change references of ad-es6-particles to cs-plugin-particles-simulator; change stash repo references to git ff0000-ad-tech repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,76 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const dotenv = require('dotenv').config()
-const isProduction = (process.env.NODE_ENV === 'production')
-const debug = require('@ff0000-ad-tech/debug')
-const log = debug('ad-es6-particles: webpack')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const dotenv = require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const debug = require("@ff0000-ad-tech/debug");
+const log = debug("cs-plugin-particle-simulator: webpack");
 
-const TARGET_DEV_PATH = dotenv.parsed.TARGET_DEV_PATH
+const TARGET_DEV_PATH = dotenv.parsed.TARGET_DEV_PATH;
 const PATHS = {
-  dist: isProduction ? path.resolve(__dirname, 'dist') : path.resolve(TARGET_DEV_PATH, 'dist'),
-  source: path.resolve(__dirname, 'source')
-}
+  dist: isProduction ? path.resolve(__dirname, "dist") : path.resolve(TARGET_DEV_PATH, "dist"),
+  source: path.resolve(__dirname, "source")
+};
 
-log(PATHS)
+log(PATHS);
 
 module.exports = {
   watch: isProduction ? false : true,
-  entry: path.resolve(PATHS.source, 'js/main.js'),
+  entry: path.resolve(PATHS.source, "js/main.js"),
   output: {
     path: PATHS.dist,
-    filename: 'bundle.js',
-    chunkFilename: '[name].js'
+    filename: "bundle.js",
+    chunkFilename: "[name].js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules\/(?!(@ff0000-ad-tech)\/).*/,
-        use: [{
-          loader: 'babel-loader',        
-          options: {
-            "presets": [
-              ["@babel/preset-env", {
-                "loose": true
-              }]
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread']
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    loose: true
+                  }
+                ]
+              ],
+              plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-object-rest-spread"]
+            }
           }
-        }]
+        ]
       }
     ]
   },
   resolve: {
-    modules: [
-      path.resolve(__dirname, 'node_modules'),
-      path.resolve(__dirname, 'node_modules/@ff0000-ad-tech')
-    ]
-  },  
+    modules: [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "node_modules/@ff0000-ad-tech")]
+  },
   plugins: [
     new CopyWebpackPlugin([
       {
-        from: path.resolve(PATHS.source, 'index.html'),
+        from: path.resolve(PATHS.source, "index.html"),
         to: PATHS.dist,
         flatten: true
       },
       {
-        from: path.resolve(PATHS.source, 'css/*'),
+        from: path.resolve(PATHS.source, "css/*"),
         to: PATHS.dist,
         flatten: true
-      }, 
+      },
       {
-        from: path.resolve(PATHS.source, 'images/*'),
-        to: path.resolve(PATHS.dist, 'images'),
+        from: path.resolve(PATHS.source, "images/*"),
+        to: path.resolve(PATHS.dist, "images"),
         flatten: true
-      }, {
-        from: path.resolve(PATHS.source, 'lib/*'),
-        to: path.resolve(PATHS.dist, 'lib'),
+      },
+      {
+        from: path.resolve(PATHS.source, "lib/*"),
+        to: path.resolve(PATHS.dist, "lib"),
         flatten: true
       }
     ])
   ],
-  devtool: 'cheap-source-map'
-}
+  devtool: "cheap-source-map"
+};
